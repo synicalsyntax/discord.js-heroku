648d9012489289e892bb63ab17945e033ad593ad
BlueSlimee <33792776+BlueSlimee@users.noreply.github.com>
2018-04-21 19:23:00 -0400

    🏓 | Ping Command!
    I added a ping command!

--- index.js
+++ index.js
@@ -6,12 +6,20 @@ client.on('ready', () => {
     client.user.setActivity('https://git.io/d.js-heroku', {type: 'WATCHING'});
 });
 
-client.on('message', msg => {
+client.on('message', async msg => {
     if (!msg.content.startsWith(process.env.PREFIX) || !msg.guild) return;
     const command = msg.content.split(' ')[0].substr(process.env.PREFIX.length);
     const args = msg.content.split(' ').slice(1).join(' ');
-    if (command === 'guide') return msg.channel.send('https://git.io/d.js-heroku');
-    else if (command === 'invite') return msg.channel.send(process.env.INVITE);
+    if (command === 'guide') {
+        msg.channel.send('https://git.io/d.js-heroku');
+    }
+    if (command === 'invite') 
+        msg.channel.send(process.env.INVITE);
+    }
+    if (command === 'ping') {
+      const pingMessage = await message.channel.send("Just a second...");
+      pingMessage.edit(":ping_pong: | Pong! `"+ pingCommand.createdTimestamp - message.createdTimestamp +"`ms.");
+    }
 });
 
 client.login(process.env.TOKEN);
