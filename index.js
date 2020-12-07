a0441301ef6d002f2d2636770067450c30b45abf
entrisity <48255412+entrisity@users.noreply.github.com>
2020-12-07 02:24:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setActivity('https://git.io/d.js-heroku', {type: 'WATCHING'});
+    client.user.setActivity('you through your window', {type: 'WATCHING'});
 });
 
 client.on('message', msg => {
